7c7e850b85b1b6b22c729df13f95c84274fd58d5
Stefan <110892250+StefanJesenko@users.noreply.github.com>
2023-09-12 08:44:12 +0200

    update

---
+++ recognition.py
@@ -0,0 +1,58 @@
+from uuid import uuid4
+
+import cv2
+from pathlib import Path
+
+from train_model import MobileNetV3RPS
+
+
+def main():
+
+    storage = {
+        0: 'rock',
+        1: 'paper',
+        2: 'scissors'
+    }
+
+    model = MobileNetV3RPS()
+
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+    window = "Acquisition"
+    cv2.namedWindow(window, cv2.WINDOW_NORMAL)
+    cv2.moveWindow(window, 0, 0)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+        frame = cv2.flip(frame, 1)  # Flip around y
+        c_y, c_x = frame.shape[0] // 2, frame.shape[1] // 2
+        capture_rec_size = 256
+        capture_rec = ((0, c_y - capture_rec_size // 2), (capture_rec_size, c_y + capture_rec_size // 2))
+
+        crop = frame.copy()[capture_rec[0][1]:capture_rec[1][1], capture_rec[0][0]:capture_rec[1][0]]
+
+        frame = cv2.rectangle(frame, capture_rec[0], capture_rec[1], (0, 0, 255))
+        frame = cv2.putText(frame, "l - login answer", (capture_rec[0][0] + 30, capture_rec[0][1] - 200),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
+        frame = cv2.putText(frame, "q - Quit", (capture_rec[0][0] + 30, capture_rec[0][1] - 50),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
+
+        if key == ord('l'):
+            crop = cv2.resize(crop, (256, 256), interpolation=cv2.INTER_AREA)
+            prediction = model(crop)
+            frame = cv2.putText(frame, f'guess: {storage[prediction]}', (capture_rec[0][0] + 30, capture_rec[0][1] - 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
+            cv2.waitKey(750)
+        cv2.imshow(window, frame)
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
